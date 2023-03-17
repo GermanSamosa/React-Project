1497178c48219530e17f9b3872eeee71c9712970
Matthew Rao <mat_rao@hotmail.com>
2023-03-17 16:08:01 -0400

    Good spot to get started
    Completed basic tutorial and deleted the fun stuff that comes with next.js

--- my-app/src/components/layout.js
+++ my-app/src/components/layout.js
@@ -1,5 +1,65 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>;
+const name = 'Matthew Rao';
+export const siteTitle = "Matthew's Portfolio";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="author" content="Matthew Rao" />
+        <meta
+          name="description"
+          content="Matthew's Portfolio 2023"
+        />
+
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
 }
